7fdf3024fe6c4e617c1aa58343fd1e32779dc96d
shenchunyang <703958897@qq.com>
2016-06-20 10:55:12 +0800

    a+b
    变量的定义不需要指定它的类型，之前学过的编程语言需要先指定变量类型，swift编程语言的编译系统会自动识别变量类型.

---
+++ a+b/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  a+b
+//
+//  Created by ry耷拉拉星球 on 16/6/20.
+//  Copyright © 2016年 ry耷拉拉星球. All rights reserved.
+//
+
+import Foundation
+
+var a = 11;
+var b = 22;
+print(a+b)
+
+//定义变量和常量
+
